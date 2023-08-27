4dfd5ab602c1dcc012d1c9ff339170489d85bbc3
fbn776 <fbn776@gmail.com>
2023-08-28 01:12:03 +0530

    initial

---
+++ src/UI/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import './app.css'
+
+
+export const App: FC = () => {
+    return (
+        <div>
+            <h1>App</h1>
+
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { TestComponent } from './App'
+import { App } from './UI/App'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TestComponent />
+    <App />
   </React.StrictMode>,
 )
