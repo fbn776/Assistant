b9776fb88e6c0230e6d0785024abaee309996889
fbn776 <fbn776@gmail.com>
2023-08-29 00:07:53 +0530

    Added tablerIcons, basics UI

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -1,12 +1,19 @@
-import { FC } from "react"
-import './app.css'
+import { FC } from "react";
+import "./app.css";
+import { IconCategory, IconSend } from "@tabler/icons-react";
 
 
 export const App: FC = () => {
-    return (
-        <div>
-            <h1>App</h1>
-
-        </div>
-    )
+	return <>
+		<section className="w-full top-0 min-h-[calc(100vh-60px)] bg-slate-50">
+			NDKM
+		</section>
+		<section className="w-full h-[60px] bg-white bottom-0 border-slate-600 border-t-2">
+			<div className="w-full h-full flex justify-center items-center p-3 gap-1">
+				<IconCategory size={"30px"}/>
+				<input type="text" className="w-full h-full outline-none border-none bg-transparent shadow-inner px-3 rounded-2xl" placeholder="Type here.."/>
+				<IconSend/>
+			</div>
+		</section>
+	</>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './UI/App'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 )
