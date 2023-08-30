c8fd7b6f545e46eda0d3c3587543a93f3208863c
fbn776 <fbn776@gmail.com>
2023-08-30 23:22:31 +0530

    UI changes

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -21,13 +21,11 @@ function convertUnixTime(unixTime: number): string {
 	const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
 	const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
 
-	return`${formattedHours}:${formattedMinutes}`;
-
+	return `${formattedHours}:${formattedMinutes}`;
 }
 
-
 const Message: FC<MessageProps> = ({ type, text, time }) => {
-	const isBot = (type === MessageType.BOT);
+	const isBot = type === MessageType.BOT;
 	return (
 		<div className={"w-full px-3 pt-3 flex" + (isBot ? "" : " justify-end")}>
 			<div
@@ -39,23 +37,24 @@ const Message: FC<MessageProps> = ({ type, text, time }) => {
 				}
 			>
 				<p>{text}</p>
-				<div className="w-full opacity-50 text-[0.60rem] text-right pt-2">{convertUnixTime(time)}</div>
+				<div className="w-full opacity-50 text-[0.60rem] text-right pt-2">
+					{convertUnixTime(time)}
+				</div>
 			</div>
 		</div>
 	);
 };
 
 export const App: FC = () => {
-
 	useEffect(() => {
 		window.onclick = () => {
 			document.body.classList.toggle("dark");
-		}
+		};
 	}, []);
 
 	return (
 		<>
-			<section className="w-full top-0 min-h-[calc(100vh-60px)] bg-l-secnd-cont-bg dark:bg-d-secnd-cont-bg">
+			<section className="w-full top-0 min-h-[calc(100dvh-var(--btmBarHeight))] bg-l-secnd-cont-bg dark:bg-d-secnd-cont-bg">
 				<Message
 					type={MessageType.BOT}
 					text="Hello there how can I help?"
@@ -82,17 +81,24 @@ export const App: FC = () => {
 					time={Date.now()}
 				/>
 			</section>
-			<section className="w-full h-[60px] bottom-0 border-l-prim-variant dark:border-d-prim-variant border-t-2 bg-l-prim-cont-bg dark:bg-d-prim-cont-bg">
-				<div className="w-full h-full flex justify-center items-center p-3 gap-3">
+			<section className="w-full h-[var(--btmBarHeight)] bottom-0 border-l-prim-variant dark:border-d-prim-variant border-t-2 bg-l-prim-cont-bg dark:bg-d-prim-cont-bg">
+				<div className="h-[40%] w-full bg-opacity-70 dark:bg-opacity-70 bg-l-prim-cont-variant dark:bg-d-prim-cont-variant text-l-prim-cont-txt dark:text-d-prim-cont-txt">
+					Hello
+				</div>
+				<div className="w-full h-[60%] flex justify-center items-center p-3 gap-3">
 					<IconCategory
 						className="stroke-l-prim-cont-txt dark:stroke-d-prim-cont-txt"
+						size={30}
 					/>
 					<input
 						type="text"
-						className="w-full h-full outline-none border-none bg-l-prim-cont-variant dark:bg-d-prim-cont-variant text-l-prim-cont-txt dark:text-d-prim-cont-txt shadow-inner px-3 rounded-2xl"
+						className="w-full h-full outline-none border-none shadow-inner px-4 py-2 rounded-full bg-l-prim-cont-variant dark:bg-d-prim-cont-variant text-l-prim-cont-txt dark:text-d-prim-cont-txt"
 						placeholder="Type here.."
 					/>
-					<IconSend className="stroke-l-prim-cont-txt dark:stroke-d-prim-cont-txt" />
+					<IconSend
+						className="stroke-l-prim-cont-txt dark:stroke-d-prim-cont-txt"
+						size={30}
+					/>
 				</div>
 			</section>
 		</>
