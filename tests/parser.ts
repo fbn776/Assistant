9b767246ef281dc7b84bfd32ca3ae08a471e1173
fbn776 <fbn776@gmail.com>
2023-09-01 00:04:13 +0530

    testing

--- tests/parser.ts
+++ tests/parser.ts
@@ -14,10 +14,10 @@ function Log<T>(
 
 /**
  * Basic splitting; splits the string based on the given delimiter. No additional process done.
- * Could be replaced by string.split(delimiter) but that would be cheating 😉
- * @param str
- * @param delimiter
- * @returns
+ * Could be replaced by `string.split(delimiter)` but that would be cheating 😉
+ * @param str string to be splitted (input)
+ * @param delimiter delimiter to split the string on
+ * @returns the splitted string as an array
  */
 function basicSplitter(str: string, delimiter: string = " "): string[] {
 	let result: string[] = [];
@@ -30,11 +30,22 @@ function basicSplitter(str: string, delimiter: string = " "): string[] {
 			temp += str[i];
 		}
 	}
-	result.push(temp);
+	temp && result.push(temp);
 
 	return result;
 }
 
+
+function getBrackets(str: string, start: string, end: string) {
+
+}
+
+/**
+ * A more advanced splitter that splits the string based on the given `delimiter` and also splits the string if there is quotes (', ", `) in the string.
+ * @param str input string
+ * @param delimiter delimiter to split the string on
+ * @returns the splitted string as an array
+ */
 function stringSplitter(str: string, delimiter: string = " "): string[] {
 	let result: string[] = [];
 	let temp = "";
@@ -50,7 +61,7 @@ function stringSplitter(str: string, delimiter: string = " "): string[] {
 
 				let nextIndex = str.indexOf(str[i], i + 1);
 
-				if (nextIndex === -1) throw new Error("No closing bracket found");
+				if (nextIndex === -1) throw new Error("No closing quote found");
 
 				result.push(str.substring(i + 1, nextIndex));
 				i = nextIndex + 1;
@@ -58,25 +69,36 @@ function stringSplitter(str: string, delimiter: string = " "): string[] {
 				if (i < str.length && str[i] != " ") throw new Error("Space not found");
 
 				continue;
-			} else {
-				result.push(temp);
-				temp = "";
 			}
+			result.push(temp);
+			temp = "";
 		} else {
 			temp += str[i];
 		}
 	}
-
+	//Only push if there is something in temp; (short circuit evaluation)
 	temp && result.push(temp);
 
 	return result;
 }
 
+function bracketSplitter(str: string, delimiter: string = " "): any {
+	let result: any = [];
+
+	for(let i=0; i < str.length; i++) {
+		
+	}
+
+	return result;
+}
+
 // Testing;
-Log("add 10 15", basicSplitter, "basic splitter");
+Log("add 10 15 ", basicSplitter, "basic splitter");
 
 Log(
-	"concat hello there 'Hii its been a good move'",
+	"concat 10 20 15 'and some text' 'and some more text' 10 argN",
 	stringSplitter,
 	"string splitter"
 );
+
+Log("add 10 20 (sum 10 (add 10 20) 20) 10", bracketSplitter, "bracket splitter");
