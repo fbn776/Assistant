5135807339aac3703dcc2c8c4f16af230f5b7084
fbn776 <fbn776@gmail.com>
2023-09-03 02:03:45 +0530

    Added more message controller functions

--- src/data/controllers/c_MessageController.ts
+++ src/data/controllers/c_MessageController.ts
@@ -1,5 +1,9 @@
 import { MessageInterface } from "../structures/s_message";
 
+/**
+ * This is a controller class for handling everything related to messages.
+ * This makes sure that the messages are handled in a centralized manner, ie this forms a single point of truth for the messages;
+ */
 export class MessageController {
 	private messages: MessageInterface[] = [];
 	private setMessages: React.Dispatch<
@@ -8,7 +12,7 @@ export class MessageController {
 
 	/**
 	 * This takes in a message state and assigns it to the controller, so that the controller alone can handle it;
-	 * @param state
+	 * @param state The return value of a useState() hook of type Array of messages
 	 */
 	setMessageState(
 		state: [
@@ -19,11 +23,33 @@ export class MessageController {
 		[this.messages, this.setMessages] = state;
 	}
 
+	/**
+	 * Adds a new message to the end
+	 * @param message the message to be added
+	 */
 	addMessage(message: MessageInterface) {
 		this.setMessages((messages) => [...messages, message]);
 	}
 
+	/**
+	 * Returns the list of all message
+	 */
 	getAllMessages() {
 		return this.messages;
 	}
+
+	/**
+	 * searches for a message with the given ID
+	 * @param id The ID of the message to be searched
+	 * @returns returns the message object if found else null
+	 */
+	getMessage(id: string): (MessageInterface | null) {
+		for(let message of this.messages) {
+			if(message.id === id)
+				return message;
+		}
+		return null;
+	}
+
+
 }
---
+++ src/parser/parser.ts
@@ -0,0 +1,5 @@
+//TODO
+
+function parse() {
+	return "haha"
+}
