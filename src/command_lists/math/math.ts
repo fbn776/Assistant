99dc1b358463b58945bf4d9d26f2f0f994c48d42
fbn776 <fbn776@gmail.com>
2023-09-15 01:52:46 +0530

    Testing

--- src/command_lists/math/math.ts
+++ src/command_lists/math/math.ts
@@ -11,3 +11,15 @@ command_registry_instance.register({
 	metadata: new Documentation("Adds twp numbers and returns the result"),
 	exec: (a, b) => a + b,
 });
+
+command_registry_instance.register({
+	name: ["sum"],
+	arguments: new ArgumentsData(-1, types.number),
+	metadata: new Documentation("Finds sum of n numbers"),
+	exec: (...rest: number[]) => {
+		let sum = 0;
+		for(let a of rest)
+			sum += a;
+		return sum;
+	}
+})
