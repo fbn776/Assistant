87c910735da4bf26985f02e00434c723e696f881
fbn776 <fbn776@gmail.com>
2023-11-19 18:17:54 +0530

    Minor change

--- src/UI/components/MessageTypes.tsx
+++ src/UI/components/MessageTypes.tsx
@@ -1,75 +1,77 @@
-import { FC } from "react";
-import { I_MessageTypeProp } from "../../data/structures/s_message";
-import { convertUnixTime } from "../../utils/utils";
+import {FC} from "react";
+import {I_MessageTypeProp} from "../../data/structures/s_message";
+import {convertUnixTime} from "../../utils/utils";
 
 /**A regular message */
 export const TextMessage: FC<I_MessageTypeProp> = (props) => {
-	return (
-		<p
-			className={
-				"message-general message-text " +
-				(props.isBot ? "bot-message" : "user-message")
-			}
-		>
-			<span className="msg-general-span">{props.data.text}</span>
-			<span className="message-time">
+    return (
+        <p
+            className={
+                "message-general message-text " +
+                (props.isBot ? "bot-message" : "user-message")
+            }
+        >
+            <span className="msg-general-span">{props.data.text}</span>
+            <span className="message-time">
 				{convertUnixTime(props.data.unixTime)}
 			</span>
-		</p>
-	);
+        </p>
+    );
 };
 
 /**A error message; same as a regular message, but the font color is red (defined at tailwind config file) */
 export const ErrorMessage: FC<I_MessageTypeProp> = (props) => {
-	return (
-		<p
-			className={
-				"message-general message-text " +
-				(props.isBot ? "bot-message" : "user-message")
-			}
-		>
-			<span className="message-error-text msg-general-span">{props.data.text}</span>
-			<span className="message-time">
+    return (
+        <div
+            className={
+                "message-general message-text " +
+                (props.isBot ? "bot-message" : "user-message")
+            }
+        >
+			<span className="message-error-text msg-general-span">
+				{props.data.text}
+			</span>
+            <span className="message-time">
 				{convertUnixTime(props.data.unixTime)}
 			</span>
-		</p>
-	);
+        </div>
+    );
 };
 
 /**An error message, but is formatted; the formatting is done in accordance with `additionalData` */
 export const FormattedErrorMessage: FC<I_MessageTypeProp> = (props) => {
-	let pos = props.data.additionalData!.position;
-	let cmd = props.data.text;
-	let start = pos - 1;
-	let end = pos + 1 >= cmd.length - 1 ? cmd.length : pos + 1;
+    let pos = props.data.additionalData!.position;
+    let cmd = props.data.text;
+    let start = pos - 1;
+    let end = pos + 1 >= cmd.length - 1 ? cmd.length : pos + 1;
 
-	let txt = (
-		<>
-			<span className="opacity-50">{cmd.substring(0, start)}</span>
-			<i className="error-marker">{cmd.substring(start, end)}</i>
-			<span className="opacity-50">{cmd.substring(end)}</span>
-		</>
-	);
+    let txt = (
+        <>
+            <span className="opacity-50">{cmd.substring(0, start)}</span>
+            <i className="error-marker">{cmd.substring(start, end)}</i>
+            <span className="opacity-50">{cmd.substring(end)}</span>
+        </>
+    );
 
-	return (
-		<p
-			className={
-				"message-general message-text " +
-				(props.isBot ? "bot-message" : "user-message")
-			}
-		>
+    return (
+        <p
+            className={
+                "message-general message-text " +
+                (props.isBot ? "bot-message" : "user-message")
+            }
+        >
 			<span className="message-error-text msg-general-span">
 				<b>{props.data.additionalData?.heading}</b>
-				<br />
+				<br/>
 				<span className="mb-[8px] block">{txt}</span>
 				<b>Reason: </b>
-				{props.data.additionalData?.errorMsg}
+                {props.data.additionalData?.errorMsg}
 			</span>
-			<span className="message-time">
+            <span className="message-time">
 				{convertUnixTime(props.data.unixTime)}
 			</span>
-		</p>
-	);
+        </p>
+    );
 };
 //   white-space: normal; /* Text wraps normally when it reaches the width */
 //   word-wrap: break-word;
